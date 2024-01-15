5f3b2c0cc7a8750d50d50feb3b9c9f1352f2f0cc
Peter Rasmussen <peter@soundboks.com>
2024-01-15 11:58:51 +0100

    more test

--- prisma/migrations/20240115082610_init/migration.sql
+++ prisma/migrations/20240115082610_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "bad_drivers" (
     "id" SERIAL NOT NULL,
-    "timestamp" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "timestamp" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
     "licensplate" CHAR(16) NOT NULL,
     "reason" VARCHAR(255) NOT NULL,
 
