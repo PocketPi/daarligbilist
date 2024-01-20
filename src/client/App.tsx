057092a75c46fb0525dfec7cc2b5b3e112fc04dd
Peter Rasmussen <peter@soundboks.com>
2024-01-20 22:18:31 +0100

    styling

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -76,6 +76,7 @@ function App (): any {
   return (
     <ThemeProvider theme={darkTheme}>
       <Container className="App" sx={{ flexDirection: 'column', display: 'flex' }}>
+        <p />
         <Typography variant="h2">{websideTitle}</Typography>
         <p />
         <Box sx={{ maxWidth: 480, alignSelf: 'center' }}>
