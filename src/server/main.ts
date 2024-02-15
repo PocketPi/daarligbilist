3358b9cd641ccc5e29abc568284e603d0450a733
Peter Rasmussen <peter@soundboks.com>
2024-02-15 12:47:20 +0100

    sanitize input

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,8 +6,40 @@ import ViteExpress from 'vite-express'
 import cors from 'cors'
 import { type BadDriverReportInterface } from '../shared/types'
 import { PrismaClient } from '@prisma/client'
+import { Validator, ValidationError, type AllowedSchema } from 'express-json-validator-middleware'
 
 dotenv.config()
+
+const { validate } = new Validator({ verbose: true })
+
+function validationErrorMiddleware(error: any, _request: any, response: any, next: any) {
+  // Check the error is a validation error
+  if (error instanceof ValidationError) {
+    // Handle the error
+    response.status(400).send(error.validationErrors)
+    next()
+  } else {
+    // Pass error on if not a validation error
+    next(error)
+  }
+}
+
+const reportBadDriverSchema: AllowedSchema = {
+  type: 'object',
+  required: ['licensplate', 'reason'],
+  properties: {
+    licensplate: {
+      type: 'string',
+      minLength: 2,
+      maxLength: 7
+    },
+    reason: {
+      type: 'string',
+      minLength: 10,
+      maxLength: 255
+    }
+  }
+}
 
 const prisma = new PrismaClient()
 
@@ -15,6 +47,7 @@ const server = express()
 
 server.use(express.json())
 server.use(cors())
+server.use(validationErrorMiddleware)
 
 server.get('/api/top10', async (_req, res) => {
   const badDrivers = await prisma.bad_drivers.groupBy({
@@ -36,7 +69,7 @@ server.get('/api/top10', async (_req, res) => {
   res.status(200).json(data)
 })
 
-server.post('/api/report_bad_driver', async (req, res, next) => {
+server.post('/api/report_bad_driver', validate({ body: reportBadDriverSchema }), async (req, res, next) => {
   try {
     const report: BadDriverReportInterface = req.body
     const result = await prisma.bad_drivers.create({
