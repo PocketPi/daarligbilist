fefa9246bcfb2b59c2e7f053630473de7e27f836
Peter Rasmussen <peter@soundboks.com>
2024-02-15 11:17:41 +0100

    remove test data api

--- src/server/main.ts
+++ src/server/main.ts
@@ -6,7 +6,7 @@ import ViteExpress from 'vite-express'
 import cors from 'cors'
 import { type BadDriverReportInterface } from '../shared/types'
 import { PrismaClient } from '@prisma/client'
-import { testData } from './GenerateTestData'
+
 dotenv.config()
 
 const prisma = new PrismaClient()
@@ -49,15 +49,6 @@ server.post('/api/report_bad_driver', async (req, res, next) => {
   } catch (error: any) {
     next(error.message)
   }
-})
-
-server.get('/api/create_test_data', async (_req, res) => {
-  await prisma.bad_drivers.createMany({
-    data: testData
-  })
-  res.status(200).json({
-    message: 'Test data created'
-  })
 })
 
 ViteExpress.listen(server, 3000, () => {
